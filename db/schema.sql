44ec1e656e4b7c358bd948ac659b35ad8c94a3e7
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-06-03 17:51:02 -0700

    Added role

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
--- server.js
+++ server.js
@@ -71,6 +71,13 @@ inquirer
 
     if (answers.choices === 'Add Role'){
       addRole();
+     
+    }
+    if (answers.choices === 'Add Employee'){
+      addEmployee();
+    }
+    if (answers.choices === 'Update Employee') {
+      updateEmployee();
     }
     
     
@@ -85,6 +92,7 @@ inquirer
     }
     )};
 
+  // function to view all roles
   const viewAllRoles = () => {
     db.query("SELECT roles.id, roles.role_title AS title, department.department_name AS department, roles.role_salary AS salary FROM roles JOIN department ON roles.department_id = department.id", (err, res) => {
       if (err) throw err;
@@ -93,6 +101,7 @@ inquirer
     })
   }
 
+  // functions to view all Employees
   const viewAllEmployees = () => {
     db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.role_title, department.department_name, roles.role_salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON department.id = roles.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id`,
     (err, res) => {
@@ -103,6 +112,7 @@ inquirer
     )
   }
 
+  // function to add department
   const addDepartment = () => {
       inquirer.prompt([
         {
@@ -120,7 +130,21 @@ inquirer
     })
   }
 
+  // Add role 
   const addRole = () => {
+    let departmentArr = [];    
+    let departmentQuery = "SELECT department_name FROM department";
+    db.query(departmentQuery, (err, res) => {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++){
+            departmentArr.push(res[i].department);
+            console.log(res)
+  }
+        let query = "SELECT roles.role_title, roles.role_salary, department.department_name FROM roles INNER JOIN department ON (roles.department_id = department.id";
+        db.query = (query, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+    
     inquirer.prompt([
       {
       type: 'input',
@@ -137,19 +161,83 @@ inquirer
       type: 'list',
       name: 'department',
       message: 'Which department does the role belong to?',
-      choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service']
+      choices: departmentArr
     }
   ]).then((answer) => {
+    let addDepartment = answer.departmentArr;
+    let addDepartmentId = departmentArr.indexOf(addDepartment);
+    addDepartmentId++;
     console.log(answer)
+    
     db.query('INSERT INTO roles SET ?',
-    {role_title: answer.role},
-    {role_salary: answer.salary},
-    {department_id: answer.department}, 
+    {role_title: answer.role ,role_salary: answer.salary, department_id: addDepartmentId}, 
     (err, res) => {
       console.table(res)
       prompt();
   
+      });
+    });            
+  })      
+  })  
+};
+    
+
+
+// Add Employee
+const addEmployee = () => {
+  let employeeArray = [];
+  db.query("SELECT roles.role_title FROM roles;", (err, res) => {
+    if (err) throw err;
+    for(let i = 0; i < res.length; i++){
+      employeeArray.push(res[i].role_title)
     }
-  )})
-  }
-  
+    
+  })
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'What is the employee\'s first name?'
+  },
+  {
+    type: 'input',
+    name: 'last_name',
+    message: 'What is the employee\'s last name?'
+},
+{
+    type: 'list',
+    name: 'role',
+    message: 'What is the employee\'s role?',
+    choices: employeeArray
+}
+// {
+//     type: 'input',
+//     name: 'manager',
+//     message: 'Who is the employee\'s manager?',
+//     choices: ""
+// }
+]).then((answer) => {
+  let addEmployeeRole = answer.role;
+  let addEmployeeId = employeeArray.indexOf(addEmployeeRole);
+  addEmployeeId++;
+  db.query('INSERT INTO employee SET ?',
+      {first_name: answer.first_name,
+       last_name: answer.last_name,
+       role_id: addEmployeeId, 
+      }, (err, res) => {
+        if (err) throw err;
+        prompt();
+})
+})
+}
+
+// update employee
+const updateEmployee = () => {
+  let updateArray = [];
+  db.query("SELECT * FROM employee", (err, res) => {
+    for(i = 0; i < res.length; i++) {
+      
+    }
+  })
+}
+
